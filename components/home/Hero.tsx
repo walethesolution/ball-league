96d3abb1f7ef5020a51e1a0aeff785d790978ada
walethesolution <olawaleayejuyone@gmail.com>
2023-07-09 00:13:40 -0400

    Add react-responsive carousel

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -3,13 +3,17 @@ import HomeCourt from "../../public/assets/images/home-court.svg";
 
 const Hero = () => {
   return (
-    <div className="">
+    <div className="relative">
       <Image
         src={HomeCourt}
         alt="this is home court"
         className="w-full h-full object-cover"
       />
-      <p>DISCOVER THE COURT OF LEGENDS</p>
+      <div className="absolute flex justify-center items-center inset-0 mt-[3rem]">
+        <p className="text-white font-bold text-2xl md:text-3xl">
+          DISCOVER THE <br className="md:hidden" /> COURT OF LEGENDS
+        </p>
+      </div>
     </div>
   );
 };
