b26e5b16b264ee9daa30d4bf2cbaa1c6befbe6de
walethesolution <olawaleayejuyone@gmail.com>
2023-07-09 01:58:43 -0400

    Add Swiper package

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,49 +1,65 @@
+"use client";
+
 import Image from "next/image";
 import HomeCourt from "../../public/assets/images/home-court.svg";
 import ThreeGuys from "../../public/assets/images/tim-fua-ima.svg";
 import Shooter from "../../public/assets/images/steve-shooter.svg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const Hero = () => {
   return (
     <>
-      <div className="relative">
-        <Image
-          src={HomeCourt}
-          alt="this is home court"
-          className="w-full h-full object-cover"
-        />
-        <div className="absolute flex justify-center items-center inset-0 mt-[3rem]">
-          <p className="text-white font-bold text-2xl md:text-3xl">
-            DISCOVER THE <br className="md:hidden" /> COURT OF LEGENDS
-          </p>
-        </div>
-      </div>
-      <div className="relative">
-        <Image
-          src={ThreeGuys}
-          alt="Three guys on court"
-          className="w-full h-full object-cover"
-        />
-        <div className="absolute flex items-center justify-center inset-0 mt-[3rem]">
-          <p className="text-white font-bold text-2xl md:text-3xl">
-            EMBRACE THE SPIRIT OF COMPETITION <br className="md:hidden" /> AND
-            CAMARADARIE
-          </p>
-        </div>
-      </div>
-      <div className="relative">
-        <Image
-          src={Shooter}
-          alt="Basketball shooting form"
-          className="w-full h-full object-cover"
-        />
-        <div className="absolute flex justify-center items-center inset-0 mt-[3rem]">
-          <p className="text-white font-bold text-2xl md:text-3xl">
-            THE SHOOTER&apos;S FORM; <br className="md:hidden" /> TAKE YOUR GAME
-            TO THE NEXT LEVEL
-          </p>
-        </div>
-      </div>
+      <Swiper>
+        <SwiperSlide>
+          <div className="relative">
+            <Image
+              src={HomeCourt}
+              alt="this is home court"
+              className="w-full h-full object-cover"
+            />
+            <div className="absolute inset-0 flex justify-center items-center  mt-[3rem]">
+              <p className="text-white font-bold text-2xl md:text-3xl text-center lg:text-5xl">
+                DISCOVER THE <br className="md:hidden" /> COURT OF LEGENDS
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <div className="relative">
+            <Image
+              src={ThreeGuys}
+              alt="Three guys on court"
+              className="w-full h-full object-cover"
+            />
+            <div className="absolute flex justify-center items-center inset-0 mt-[5rem]">
+              <p className="text-white font-bold text-2xl md:text-3xl text-center lg:text-5xl">
+                EMBRACE THE SPIRIT OF COMPETITION <br /> AND CAMARADARIE
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <div className="relative">
+            <Image
+              src={Shooter}
+              alt="Basketball shooting form"
+              className="w-full h-full object-cover"
+            />
+            <div className="absolute flex justify-center items-center inset-0 mt-[4rem]">
+              <p className="text-white font-bold text-2xl md:text-3xl text-center lg:text-5xl">
+                THE SHOOTER&apos;S FORM, <br />
+                TAKE YOUR GAME TO THE NEXT LEVEL
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </>
   );
 };
