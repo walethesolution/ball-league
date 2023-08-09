882232c2028745c1f8bbf418daf22225b900aaac
walethesolution <olawaleayejuyone@gmail.com>
2023-08-09 12:54:47 -0400

    Add close menu function

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -14,6 +14,10 @@ const Header: React.FC = () => {
     setMenuOpen(!menuOpen);
   };
 
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   useEffect(() => {
     const handleResize = () => {
       // close menu when screensize reaches breakpoints
@@ -37,6 +41,7 @@ const Header: React.FC = () => {
               className="w-[61px] h-[64px] md:w-[85px] md:h-[72px] lg:w-[137px] lg:h-[113px]"
               src={Logo}
               alt="Kings league logo"
+              onClick={closeMenu}
             />
           </Link>
           <div className="lg:hidden absolute left-1/2 transform -translate-x-1/2">
@@ -62,7 +67,7 @@ const Header: React.FC = () => {
         </ul>
         <Link
           href={""}
-          className=" hidden lg:block   bg-[#2A6041] mr-8 text-white px-7 py-2.5 text-sm font-bold rounded-xl xl:px-7 xl:py-2.5 xl:text-base"
+          className=" hidden lg:block bg-[#2A6041] mr-8 text-white px-7 py-2.5 text-sm font-bold rounded-xl xl:px-7 xl:py-2.5 xl:text-base"
         >
           Sign up
         </Link>
@@ -77,24 +82,38 @@ const Header: React.FC = () => {
       {menuOpen && (
         <div className="absolute right-0 w-3/4 top-[83px] p-2 shadow z-[9999] bg-[#FFFFFA] h-screen ">
           <ul className="flex flex-col mx-4 gap-4">
-            <Link href={"/games"} className="mb-2 text-lg font-normal md:mb-4">
+            <Link
+              href={"/games"}
+              className="mb-2 text-lg font-normal md:mb-4"
+              onClick={closeMenu}
+            >
               GAMES
             </Link>
-            <Link href={"/about"} className="mb-2 text-lg font-normal md:mb-4">
+            <Link
+              href={"/about"}
+              className="mb-2 text-lg font-normal md:mb-4"
+              onClick={closeMenu}
+            >
               ABOUT US
             </Link>
             <Link
               href={"/players"}
               className="mb-2 text-lg font-normal md:mb-4"
+              onClick={closeMenu}
             >
               PLAYERS
             </Link>
-            <Link href={"/games"} className="mb-2 text-lg font-normal md:mb-4">
+            <Link
+              href={"/games"}
+              className="mb-2 text-lg font-normal md:mb-4"
+              onClick={closeMenu}
+            >
               STATS
             </Link>
             <Link
               href={"/updates"}
               className="mb-2 text-lg font-normal md:mb-4"
+              onClick={closeMenu}
             >
               NEWS & BLOG
             </Link>
