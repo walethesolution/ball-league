1a944eed8fbc7f89face237d79e16045f2ad499d
walethesolution <olawaleayejuyone@gmail.com>
2023-11-13 22:02:11 -0500

    Increase font size on lg devices

--- components/register-page/Index.tsx
+++ components/register-page/Index.tsx
@@ -3,9 +3,9 @@ import SignIn from "./SignIn";
 const Index = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-4 lg:flex-row lg:justify-evenly">
-      <div className="flex flex-col gap-2 text-center">
-        <h1 className="font-bold text-2xl">KINGS LEAGUE</h1>
-        <p className="text-base">
+      <div className="flex flex-col gap-2 text-center lg:gap-4">
+        <h1 className="font-bold text-2xl lg:text-3xl">KINGS LEAGUE</h1>
+        <p className="text-base lg:text-lg">
           Be a part of the Ball & Life family. <br /> A catalyst for positive
           change through <br />
           the game you love.
