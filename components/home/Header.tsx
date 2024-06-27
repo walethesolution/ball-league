27b97d15aac21d45b42ae8ce98633df5e3e0e1ae
walethesolution <olawaleayejuyone@gmail.com>
2024-06-27 02:43:57 -0400

    Change Hero Orientation

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -6,7 +6,6 @@ import Link from "next/link";
 import Logo from "../../public/assets/images/logo.svg";
 import Hamburger from "../../public/assets/icons/hamburger.svg";
 import CloseIcon from "../../public/assets/icons/close-icon.svg";
-import NavLink from "./NavLink";
 
 const Header: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -26,8 +25,6 @@ const Header: React.FC = () => {
   };
 
   useEffect(() => {
-    localStorage.removeItem("activeLink");
-
     const storedActiveLink = localStorage.getItem("activeLink");
     if (storedActiveLink) {
       setActiveLink(storedActiveLink);
@@ -50,9 +47,23 @@ const Header: React.FC = () => {
     localStorage.setItem("activeLink", activeLink);
   }, [activeLink]);
 
+  useEffect(() => {
+    // Prevent scrolling when the menu is open
+    if (menuOpen) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+
+    // Clean up on component unmount
+    return () => {
+      document.body.classList.remove("no-scroll");
+    };
+  }, [menuOpen]);
+
   return (
     <div>
-      <div className="w-full fixed z-[9999] bg-[#FFFFFA] shadow-lg">
+      <div className="w-full fixed bg-transparent shadow-lg z-50">
         <nav className="flex items-center justify-between mx-3 h-[85px]">
           <div className="flex items-center">
             <Link href={"/"}>
@@ -63,63 +74,13 @@ const Header: React.FC = () => {
                 onClick={handleLogoClick}
               />
             </Link>
-            <div className="lg:hidden absolute left-1/2 transform -translate-x-1/2">
-              <h2 className="text-xl text-black font-lobster font-semibold">
-                KINGS LEAGUE
+            <div className="absolute left-1/2 transform -translate-x-1/2">
+              <h2 className="text-3xl text-black font-lobster font-semibold">
+                KL
               </h2>
             </div>
           </div>
-          <ul className="hidden lg:flex items-center space-x-16 lg:justify-center">
-            <NavLink
-              href={"/games"}
-              activeLink={activeLink}
-              setActiveLink={setActiveLink}
-            >
-              GAMES
-            </NavLink>
-            <NavLink
-              href={"/about"}
-              activeLink={activeLink}
-              setActiveLink={setActiveLink}
-            >
-              ABOUT US
-            </NavLink>
-            <NavLink
-              href={"/players"}
-              activeLink={activeLink}
-              setActiveLink={setActiveLink}
-            >
-              PLAYERS
-            </NavLink>
-            <NavLink
-              href={"/stats"}
-              activeLink={activeLink}
-              setActiveLink={setActiveLink}
-            >
-              STATS
-            </NavLink>
-            <NavLink
-              href={"/updates"}
-              activeLink={activeLink}
-              setActiveLink={setActiveLink}
-            >
-              NEWS & BLOG
-            </NavLink>
-            <NavLink
-              href={"/events"}
-              activeLink={activeLink}
-              setActiveLink={setActiveLink}
-            >
-              EVENTS
-            </NavLink>
-          </ul>
-          <Link
-            href={"/login"}
-            className=" hidden lg:block bg-[#2A6041] mr-8 text-white px-7 py-2.5 text-sm font-bold rounded-xl xl:px-7 xl:py-2.5 xl:text-base"
-          >
-            Log in
-          </Link>
-          <div className="lg:hidden cursor-pointer" onClick={handleMenuToggle}>
+          <div className="cursor-pointer lg:mr-4" onClick={handleMenuToggle}>
             {menuOpen ? (
               <Image src={CloseIcon} alt="close-icon" />
             ) : (
@@ -129,57 +90,50 @@ const Header: React.FC = () => {
         </nav>
       </div>
       {menuOpen && (
-        <div className="fixed right-0 w-3/4 top-[80px] p-2 shadow bg-[#FFFFFA] h-screen z-[9999] flex flex-col gap-24">
-          <ul className="flex flex-col mx-4 gap-4">
+        <div className="fixed right-0 w-1/3 top-[80px] p-2 shadow bg-[#FFFFFA] h-screen z-[9999] flex flex-col gap-24 transition-transform duration-500 ease-in-out">
+          <ul className="flex flex-col mx-4 text-xl gap-4">
             <Link
               href={"/games"}
               className="mb-2 text-lg font-normal md:mb-4"
-              onClick={closeMenu}
-            >
+              onClick={closeMenu}>
               GAMES
             </Link>
             <Link
               href={"/about"}
               className="mb-2 text-lg font-normal md:mb-4"
-              onClick={closeMenu}
-            >
+              onClick={closeMenu}>
               ABOUT US
             </Link>
             <Link
               href={"/players"}
               className="mb-2 text-lg font-normal md:mb-4"
-              onClick={closeMenu}
-            >
+              onClick={closeMenu}>
               PLAYERS
             </Link>
             <Link
               href={"/stats"}
               className="mb-2 text-lg font-normal md:mb-4"
-              onClick={closeMenu}
-            >
+              onClick={closeMenu}>
               STATS
             </Link>
             <Link
               href={"/updates"}
               className="mb-2 text-lg font-normal md:mb-4"
-              onClick={closeMenu}
-            >
+              onClick={closeMenu}>
               NEWS & BLOG
             </Link>
             <Link
               href={"/events"}
               className="mb-2 text-lg font-normal md:mb-4"
-              onClick={closeMenu}
-            >
+              onClick={closeMenu}>
               EVENTS
             </Link>
           </ul>
           <Link
             href={"login"}
             className="bg-[#2A6041] text-white px-7 py-2.5 text-sm font-bold rounded-xl xl:px-7 xl:py-2.5 xl:text-base flex items-center justify-center"
-            onClick={closeMenu}
-          >
-            Log in
+            onClick={closeMenu}>
+            LOG IN
           </Link>
         </div>
       )}
--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -5,19 +5,21 @@ import HomeCourt from "../../public/assets/images/homeCourt.png";
 
 const Hero: React.FC = () => {
   return (
-    <div>
+    <>
       <Image
         src={HomeCourt}
         alt="this is home court"
         className="w-full h-screen object-cover"
       />
-      <div className="absolute inset-0 flex justify-center items-center mt-[3rem] fade-in">
-        <p className="text-white font-bold text-2xl md:text-3xl text-center lg:text-5xl">
-          DISCOVER <br className="md:hidden" /> THE <br className="md:hidden" />
-          COURT OF LEGENDS
-        </p>
+      <div className="absolute inset-0 flex flex-col justify-center items-center mt-[3rem] fade-in">
+        <div
+          className="text-center text-white text-[12.375vw] md-[9.375vw]"
+          style={{ lineHeight: "0.8em" }}>
+          <p>Kings</p>
+          <p>League</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
